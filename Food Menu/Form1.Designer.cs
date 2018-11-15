b2386765f0c094e3b5c9ca2755bd6e52d7c2b43e
Romeo <lihaha1@yahoo.com.hk>
2018-11-15 19:10:24 +0800

    importing project

---
+++ Food Menu/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace Food_Menu
+{
+    partial class frmMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnDrink = new System.Windows.Forms.Label();
+            this.btnFood = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.lblTotalPrice = new System.Windows.Forms.Label();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnPlus = new System.Windows.Forms.Button();
+            this.btnRight = new System.Windows.Forms.Button();
+            this.btnLeft = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.panel1.Controls.Add(this.lblPrice);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.lblInfo);
+            this.panel1.Controls.Add(this.lblName);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.btnDrink);
+            this.panel1.Controls.Add(this.btnFood);
+            this.panel1.Location = new System.Drawing.Point(28, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(512, 296);
+            this.panel1.TabIndex = 0;
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Location = new System.Drawing.Point(429, 254);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(17, 12);
+            this.lblPrice.TabIndex = 6;
+            this.lblPrice.Text = "$$";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Reem Kufi", 8.999999F);
+            this.label5.Location = new System.Drawing.Point(309, 247);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 23);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Price";
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.Location = new System.Drawing.Point(311, 75);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(158, 89);
+            this.lblInfo.TabIndex = 4;
+            this.lblInfo.Text = "Infomation";
+            this.lblInfo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(374, 26);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(32, 12);
+            this.lblName.TabIndex = 3;
+            this.lblName.Text = "Name";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(20, 41);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(252, 241);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnDrink
+            // 
+            this.btnDrink.BackColor = System.Drawing.Color.Aquamarine;
+            this.btnDrink.Location = new System.Drawing.Point(147, 3);
+            this.btnDrink.Name = "btnDrink";
+            this.btnDrink.Size = new System.Drawing.Size(123, 35);
+            this.btnDrink.TabIndex = 1;
+            this.btnDrink.Text = "Drink";
+            this.btnDrink.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnFood
+            // 
+            this.btnFood.BackColor = System.Drawing.Color.Aquamarine;
+            this.btnFood.ForeColor = System.Drawing.Color.Black;
+            this.btnFood.Location = new System.Drawing.Point(18, 3);
+            this.btnFood.Name = "btnFood";
+            this.btnFood.Size = new System.Drawing.Size(123, 35);
+            this.btnFood.TabIndex = 0;
+            this.btnFood.Text = "Food";
+            this.btnFood.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.panel3.Controls.Add(this.lblTotalPrice);
+            this.panel3.Controls.Add(this.dgv);
+            this.panel3.Location = new System.Drawing.Point(588, 12);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(200, 426);
+            this.panel3.TabIndex = 1;
+            // 
+            // lblTotalPrice
+            // 
+            this.lblTotalPrice.Location = new System.Drawing.Point(40, 356);
+            this.lblTotalPrice.Name = "lblTotalPrice";
+            this.lblTotalPrice.Size = new System.Drawing.Size(117, 37);
+            this.lblTotalPrice.TabIndex = 2;
+            this.lblTotalPrice.Text = "Total Price";
+            this.lblTotalPrice.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // dgv
+            // 
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Location = new System.Drawing.Point(3, 3);
+            this.dgv.Name = "dgv";
+            this.dgv.RowTemplate.Height = 24;
+            this.dgv.Size = new System.Drawing.Size(194, 320);
+            this.dgv.TabIndex = 0;
+            this.dgv.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.panel2.Controls.Add(this.btnPlus);
+            this.panel2.Controls.Add(this.btnRight);
+            this.panel2.Controls.Add(this.btnLeft);
+            this.panel2.Location = new System.Drawing.Point(28, 338);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(512, 100);
+            this.panel2.TabIndex = 1;
+            // 
+            // btnPlus
+            // 
+            this.btnPlus.Font = new System.Drawing.Font("MS UI Gothic", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnPlus.Location = new System.Drawing.Point(349, 20);
+            this.btnPlus.Name = "btnPlus";
+            this.btnPlus.Size = new System.Drawing.Size(150, 60);
+            this.btnPlus.TabIndex = 3;
+            this.btnPlus.Text = "+";
+            this.btnPlus.UseVisualStyleBackColor = true;
+            this.btnPlus.Click += new System.EventHandler(this.btnPlus_Click);
+            // 
+            // btnRight
+            // 
+            this.btnRight.Font = new System.Drawing.Font("MS UI Gothic", 40F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnRight.Location = new System.Drawing.Point(181, 20);
+            this.btnRight.Name = "btnRight";
+            this.btnRight.Size = new System.Drawing.Size(150, 60);
+            this.btnRight.TabIndex = 2;
+            this.btnRight.Text = "⇨";
+            this.btnRight.UseVisualStyleBackColor = true;
+            this.btnRight.Click += new System.EventHandler(this.btnRight_Click);
+            // 
+            // btnLeft
+            // 
+            this.btnLeft.Font = new System.Drawing.Font("MS UI Gothic", 40F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnLeft.Location = new System.Drawing.Point(20, 20);
+            this.btnLeft.Name = "btnLeft";
+            this.btnLeft.Size = new System.Drawing.Size(150, 60);
+            this.btnLeft.TabIndex = 1;
+            this.btnLeft.Text = "⇦";
+            this.btnLeft.UseVisualStyleBackColor = true;
+            this.btnLeft.Click += new System.EventHandler(this.btnLeft_Click);
+            // 
+            // frmMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkCyan;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "frmMenu";
+            this.Text = "Menu";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label btnDrink;
+        private System.Windows.Forms.Label btnFood;
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblTotalPrice;
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.Button btnPlus;
+        private System.Windows.Forms.Button btnRight;
+        private System.Windows.Forms.Button btnLeft;
+    }
+}
+
---
+++ Food Menu/Form1.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Food_Menu
+{
+    public partial class frmMenu : Form
+    {
+        private List<Product> ListOfFood = new List<Product>();
+        private int FoodIndex = 0;
+        private bool IsFoodTab = true;
+        private List<Product> ListOfDrink = new List<Product>();
+        private int DrinkIndex = 0;
+
+        public frmMenu()
+        {
+            InitializeComponent();
+
+            BuildProductList();
+
+            SetEvent();
+
+            DisplayFood();
+
+            SetDgv();
+        }
+
+        private void BuildProductList()
+        {
+            Product product = new Product("Plzza", "Italian Pizza from Vietnam", 1500, Properties.Resources.Pizza, true);
+            ListOfFood.Add(product);
+            product = new Product("Sushi", "Japanese Sushi from Osaka", 200, Properties.Resources.Sushi, true);
+            ListOfFood.Add(product);
+            product = new Product("Ramen", "Japanese Ramen from Hongkong", 800, Properties.Resources.ramen, true);
+            ListOfFood.Add(product);
+            product = new Product("Coca Cola", "Famous softdrink from USA", 150, Properties.Resources.CocaCola, false);
+            ListOfDrink.Add(product);
+            product = new Product("Lemon Orange Juice", "Heathly drink from UK", 100, Properties.Resources.Juice, false);
+            ListOfDrink.Add(product);
+        }
+
+        private void DisplayFood()
+        {
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.Image = ListOfFood[FoodIndex].TheImage;
+            lblName.Text = ListOfFood[FoodIndex].Name;
+            lblInfo.Text = ListOfFood[FoodIndex].Information;
+            lblPrice.Text = ListOfFood[FoodIndex].Price.ToString("C");
+        }
+
+        private void DisplayDrink()
+        {
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.Image = ListOfDrink[DrinkIndex].TheImage;
+            lblName.Text = ListOfDrink[DrinkIndex].Name;
+            lblInfo.Text = ListOfDrink[DrinkIndex].Information;
+            lblPrice.Text = ListOfDrink[DrinkIndex].Price.ToString("C");
+        }
+
+        private void SetEvent()
+        {
+            btnFood.Click += TanBtnTab_Click;
+            btnDrink.Click += TanBtnTab_Click;
+        }
+
+        private void TanBtnTab_Click(object sender, EventArgs e)
+        {
+            Label btn = (Label)sender;
+            if (btn.Name == (btnFood.Name) && !IsFoodTab)
+            {
+                btnFood.BackColor = Color.Aquamarine;
+                btnFood.ForeColor = Color.Black;
+                btnDrink.BackColor = Color.LightGreen;
+                btnDrink.ForeColor = Color.White;
+                IsFoodTab = true;
+                DisplayFood();
+            }
+            else if (btn.Name == (btnDrink.Name) && IsFoodTab)
+            {
+                btnFood.BackColor = Color.LightGreen;
+                btnFood.ForeColor = Color.White;
+                btnDrink.BackColor = Color.Aquamarine;
+                btnDrink.ForeColor = Color.Black;
+                IsFoodTab = false;
+                DisplayDrink();
+            }
+        }
+
+        private void btnLeft_Click(object sender, EventArgs e)
+        {
+            if (IsFoodTab)
+            {
+                FoodIndex++;
+                //超過INDEX就返0
+                if (FoodIndex >= ListOfFood.Count) FoodIndex = 0;
+                DisplayFood();
+            }
+
+            else
+            {
+                DrinkIndex++;
+                //超過INDEX就返0
+                if (DrinkIndex >= ListOfDrink.Count) DrinkIndex = 0;
+                DisplayDrink();
+            }
+        }
+
+        private void btnRight_Click(object sender, EventArgs e)
+        {
+            if (IsFoodTab)
+            {
+                FoodIndex--;
+                if (FoodIndex <= -1) FoodIndex = ListOfFood.Count - 1;
+                DisplayFood();
+            }
+            else
+            {
+                DrinkIndex--;
+                if (DrinkIndex <= -1) DrinkIndex = ListOfDrink.Count - 1;
+                DisplayDrink();
+            }
+        }
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            int rowIdx = 0;
+
+            if (IsFoodTab)
+            {
+                // we are at food tab
+                if (CheckExistOnDgvAndAddMore(ListOfFood[FoodIndex].Name) == false)
+                {
+                    dgv.Rows.Add();
+
+                    rowIdx = dgv.Rows.Count - 1;
+                    dgv.Rows[rowIdx].Height = 40;
+                    dgv.Rows[rowIdx].Cells["ProductNm"].Value = ListOfFood[FoodIndex].Name;
+                    dgv.Rows[rowIdx].Cells["Amount"].Value = 1;
+                    dgv.Rows[rowIdx].Cells["Price"].Value = ListOfFood[FoodIndex].Price;
+                    dgv.Rows[rowIdx].Cells[3].Value = "DEL";
+
+                    dgv.ClearSelection();
+                }
+            }
+            else
+            {
+                // we are at drink tab
+                if (CheckExistOnDgvAndAddMore(ListOfDrink[DrinkIndex].Name) == false)
+                {
+                    dgv.Rows.Add();
+                    rowIdx = dgv.Rows.Count - 1;
+                    dgv.Rows[rowIdx].Height = 40;
+                    dgv.Rows[rowIdx].Cells["ProductNm"].Value = ListOfDrink[DrinkIndex].Name;
+                    dgv.Rows[rowIdx].Cells["Amount"].Value = 1;
+                    dgv.Rows[rowIdx].Cells["Price"].Value = ListOfDrink[DrinkIndex].Price;
+                    dgv.Rows[rowIdx].Cells[3].Value = "DEL";
+
+                    dgv.ClearSelection();
+                }
+            }
+
+            if (rowIdx % 2 == 0)
+            {
+                dgv.Rows[rowIdx].DefaultCellStyle.BackColor = Color.HotPink;
+            }
+            else
+            {
+                dgv.Rows[rowIdx].DefaultCellStyle.BackColor = Color.HotPink;
+            }
+
+            dgv.Rows[rowIdx].DefaultCellStyle.ForeColor = Color.White;
+
+            SumAllPrice();
+        }
+
+        private void SumAllPrice()
+        {
+            if (dgv.Rows.Count > 0)
+            {
+                int sum = 0;
+                for (int index = 0; index < dgv.Rows.Count; index++)
+                {
+                    sum += Convert.ToInt32(dgv.Rows[index].Cells["Price"].Value);
+                }
+
+                lblTotalPrice.Text = sum.ToString();
+            }
+            else
+            {
+                lblTotalPrice.Text = "0";
+            }
+        }
+
+        private bool CheckExistOnDgvAndAddMore(string productName)
+        {
+            bool ret = false;
+            for (int index = 0; index < dgv.Rows.Count; index++)
+            {
+                if (dgv.Rows[index].Cells["ProductNm"].Value.ToString() == (productName))
+                {
+                    int amountOrder = int.Parse(dgv.Rows[index].Cells["Amount"].Value.ToString()) + 1;
+                    dgv.Rows[index].Cells["Price"].Value = amountOrder * Convert.ToInt32(IsFoodTab ? ListOfFood[FoodIndex].Price
+                        : ListOfDrink[DrinkIndex].Price);
+                    dgv.Rows[index].Cells["Amount"].Value = amountOrder;
+                    ret = true;
+                    break;
+                }
+            }
+            return ret;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // set e to find help the situation
+            int colIdx = e.ColumnIndex;
+            int rowIdx = e.RowIndex;
+
+            if (colIdx == 3 && rowIdx >= 0)
+            {
+                int amount = int.Parse(dgv.Rows[rowIdx].Cells["Amount"].Value.ToString());
+                int unitPrice = int.Parse(dgv.Rows[rowIdx].Cells["Price"].Value.ToString()) / amount;
+                amount--;
+
+                if (amount == 0)
+                {
+                    dgv.Rows.RemoveAt(rowIdx);
+                }
+                else
+                {
+                    dgv.Rows[rowIdx].Cells["Amount"].Value = amount;
+                    dgv.Rows[rowIdx].Cells["Price"].Value = amount * unitPrice;
+                }
+            }
+            SumAllPrice();
+        }
+
+        
+
+        private void SetDgv()
+        {
+            dgv.AllowUserToAddRows = false;
+            dgv.AllowUserToDeleteRows = false;
+            dgv.AllowUserToResizeColumns = false;
+            dgv.AllowUserToOrderColumns = false;
+            dgv.AllowUserToResizeRows = false;
+            dgv.RowHeadersVisible = false;
+            dgv.ReadOnly = true;
+            dgv.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.Single;
+            dgv.SelectionMode = DataGridViewSelectionMode.RowHeaderSelect;
+
+            dgv.Columns.Add("ProductNm", "Name");
+            dgv.Columns.Add("Amount", "No");
+            dgv.Columns.Add("Price", "Amount");
+            DataGridViewButtonColumn dbc = new DataGridViewButtonColumn();
+            dgv.Columns.Add(dbc);
+            dgv.Columns.Add("Remove", "Del");
+            dgv.Columns["ProductNm"].Width = 70;
+            dgv.Columns["ProductNm"].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dgv.Columns["ProductNm"].DefaultCellStyle.Font = new Font("Tahoma", 10.0f);
+            dgv.Columns["ProductNm"].HeaderCell.Style.Font = new Font("Tahoma", 10.0f);
+            dgv.Columns["Amount"].Width = 30;
+            dgv.Columns["Amount"].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+            dgv.Columns["Amount"].DefaultCellStyle.Font = new Font("Tahoma", 10.0f);
+            dgv.Columns["Amount"].HeaderCell.Style.Font = new Font("Tahoma", 10.0f);
+            dgv.Columns["Price"].Width = 70;
+            dgv.Columns["Price"].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+            dgv.Columns["Price"].DefaultCellStyle.Font = new Font("Tahoma", 10.0f);
+            dgv.Columns["Price"].HeaderCell.Style.Font = new Font("Tahoma", 10.0f);
+            dgv.Columns[3].Width = 25;
+        }
+    }
+
+
+    public struct Product
+    {
+        public string Name;
+        public string Information;
+        public int Price;
+        public Image TheImage;
+        public bool IsFoodNotDrink;
+
+        public Product(string pName, string pInformation, int pPrice, Image pImage, bool IsFood)
+        {
+            Name = pName;
+            Information = pInformation;
+            Price = pPrice;
+            TheImage = pImage;
+            IsFoodNotDrink = IsFood;
+        }
+    }
+
+
+}
